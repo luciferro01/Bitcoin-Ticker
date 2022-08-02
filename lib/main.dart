bd9a31612c2f6b9af462b2773d116ba8915a3020
luciferro01 <lucifer1111ronaldo@gmail.com>
2022-08-03 00:02:38 +0530

    Adding api functionality error: rate has an error in line 45 in initializing it

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: unnecessary_const
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'coin.dart';
 import 'dart:io' show Platform;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'coin.dart';
 
+const apiKey = '41ACA103-11C3-44E2-A9A7-1C471BD8706B';
 void main() {
   runApp(
     const MyApp(),
@@ -17,19 +20,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: BitcoinTicker(),
+      home: const BitcoinTicker(),
     );
   }
 }
 
 class BitcoinTicker extends StatefulWidget {
+  const BitcoinTicker({Key? key}) : super(key: key);
+
   @override
   State<BitcoinTicker> createState() => _BitcoinTickerState();
 }
 
 class _BitcoinTickerState extends State<BitcoinTicker> {
   // const BitcoinTicker({Key? key}) : super(key: key);
-  String selectedCurrency = 'INR';
+
+  @override
+  void initState() {
+    super.initState();
+    getrates();
+  }
+
+  String currency = 'USD';
+  late double rate = 845;
+
+  void getrates() async {
+    String url =
+        'https://rest.coinapi.io/v1/exchangerate/BTC/$currency?apiKey=$apiKey';
+
+    http.Response response = await http.get(Uri.parse(url));
+    var data = response.body;
+    var decodeData = jsonDecode(data);
+    // print(decodeData['rate']);
+    rate = await decodeData['rate'];
+  }
+
+  String selectedCurrency = 'USD';
 
   DropdownButton androidPicker() {
     List<DropdownMenuItem<String>> dropDownItems = [];
@@ -48,6 +74,8 @@ class _BitcoinTickerState extends State<BitcoinTicker> {
         // print(value);
         setState(() {
           selectedCurrency = value!;
+          currency = value;
+          getrates();
         });
       },
     );
@@ -128,7 +156,7 @@ class _BitcoinTickerState extends State<BitcoinTicker> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      selectedCurrency,
+                      'BTC ==  $selectedCurrency => ${rate.toStringAsFixed(4)}',
                       style: const TextStyle(fontSize: 20),
                     ),
                   ],
